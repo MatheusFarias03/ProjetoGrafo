4db9c4b00740eeb9bbce4df14419552b9f7ec4b6
Matheus <matheusfarias519@gmail.com>
2023-09-18 23:18:06 -0300

    Wholesalers are stored as vertices

--- src/RappiWebScraper.py
+++ src/RappiWebScraper.py
@@ -1,6 +1,7 @@
 from bs4 import BeautifulSoup
 import requests
 import json
+from Graph import *
 
 class RappiWebScraper:
 
@@ -47,7 +48,12 @@ class RappiWebScraper:
             print(f'Error: {e}.')
 
 
-    def get_wholesalers_names(self):
+    def get_wholesalers_vertices(self, id_num:int):
+        
+        wholesalers_vertices = []
+        
+        self.get_wholesalers_links()
+        
         if len(self.wholesalers_links) != 0:
             try:
                 # For each site, send a GET request and scrap it.
@@ -59,14 +65,60 @@ class RappiWebScraper:
                     wholesaler_name = soup.find("h1", {"data-qa":"store-name"})
                     wholesaler_name = wholesaler_name.get_text()
                     self.wholesalers_names.append(wholesaler_name)
-
-                    print(f'\nWebsite: {link} \nWholesaler Name: {wholesaler_name}')
+                    
+                    # Create a vertex for the wholesaler.
+                    wholesaler_vertex = Vertex(id=id_num, properties={'name': wholesaler_name, 'link': link}, label="Wholesaler")
+                    wholesalers_vertices.append(wholesaler_vertex)
+                    id_num += 1
+                    
+                print("Wholesalers vertices have been successfully created.")
 
             except Exception as e:
                 print(f'Error: {e}.')
+        
+        else:
+            print("Wholesalers list empty!")
+        
+        return wholesalers_vertices
+    
+    
+    def get_wholesalers_fish(self, wholesalers_vertices, id_num:int):
+        
+        product_vertices = []
+        
+        if len(wholesalers_vertices) != 0:
+            try:
+                for v in wholesalers_vertices:
+                    fish_link = v.properties['link'] + '/acougue-e-peixaria/peixes'
+                    self.html_content = requests.get(fish_link)
+                    soup = BeautifulSoup(self.html_content.text, 'html.parser')
+                    
+                    # Find all the div elements with class "sc-es0vli ilCBrd" which represents each product.
+                    div_elements = soup.find_all('div', {"class": "sc-esOvli.ilCBrd"})
+                    print(div_elements)
+                    
+                    # Loop through the div elements and extract the content
+                    for div in div_elements:
+                        product_name = div.find('div', {'data-qa': 'product-name'})
+                        product_description = div.find('span', {'data-qa': 'product-description'})
+                        product_price = div.find('span', {'data-qa': 'product-price'})
+                        
+                        if product_name and product_description and product_price:
+                            product = Vertex(id=id_num, properties={'name': product_name, 
+                                                                    'description': product_description, 
+                                                                    'price': product_price})
+                            product_vertices.append(product)
+                            id_num += 1
+                            
+                print("Some fish were caught!")
+            
+            except Exception as e:
+                print(f'Error {e}.')
+                
+        else:
+            print("Wholesalers list empty!")
+            
+        return product_vertices
 
-
-rappi_scraper = RappiWebScraper()
-print(rappi_scraper)
-rappi_scraper.get_wholesalers_links()
-rappi_scraper.get_wholesalers_names()
+            
+            
--- src/main.py
+++ src/main.py
@@ -1,8 +1,21 @@
 from Graph import *
+from RappiWebScraper import *
 import tests
 
 def main():
+    graph = Graph("ProductsRecommender")
+    rappi_scraper = RappiWebScraper()
     
-    tests.test_two()
-
+    id_num = 1
+    wholesalers_vertices = rappi_scraper.get_wholesalers_vertices(id_num=id_num)
+    for v in wholesalers_vertices:
+        graph.add_vertex(v)
+        
+    graph.show_adj_matrix()
+    
+    fish_vertices = rappi_scraper.get_wholesalers_fish(wholesalers_vertices, id_num)
+    for v in fish_vertices:
+        graph.add_vertex(v)
+        
+    graph.show_adj_matrix()
 main()
